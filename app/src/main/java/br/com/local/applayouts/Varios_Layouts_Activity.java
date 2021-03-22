19d7cf836a5d932073f44e22a1ab26d4df7995a9
Laercio-Note <lndsilva@hotmail.com>
2021-03-22 19:36:03 -0300

    Diversos layouts

---
+++ app/src/main/java/br/com/local/applayouts/Varios_Layouts_Activity.java
@@ -0,0 +1,14 @@
+package br.com.local.applayouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Varios_Layouts_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.varios_layout);
+    }
+}
